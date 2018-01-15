09de0ebefb70d29235394f7a36d772e807584aee
nina <ninakartina.nina@gmail.com>
2018-01-15 14:18:35 +0700

    edit level 1,2,3

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+//LEVEL 0
 const sentenceA = "Hello World!"
 const sentenceB = "Good Bye!"
 
 console.log(sentenceA)
 console.log(sentenceB)
+
+//LEVEL 1
+const sentenceC = sentenceA + " " + sentenceB
+console.log(sentenceC);
+
+const sentenceD = `${sentenceA} ${sentenceB}`
+console.log(sentenceC);
+
+//LEVEL 2
+console.log(sentenceA.toUpperCase());
+console.log(sentenceB.toUpperCase());
+console.log(sentenceA.toLowerCase());
+console.log(sentenceB.toLowerCase());
+
+//LEVEL 3
+const sentenceE = "hello goodness!"
+console.log(sentenceE)
+console.log(sentenceE.toTitleCase());
