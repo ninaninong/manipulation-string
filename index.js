b06c420e434af40e187202ab45f71e882a69c696
nina <ninakartina.nina@gmail.com>
2018-01-16 13:46:18 +0700

    update level 3

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ console.log(sentenceA.toLowerCase());
 console.log(sentenceB.toLowerCase());
 
 //LEVEL 3
-const sentenceE = "hello goodness!"
-console.log(sentenceE)
-
-function toTitleCase(str) {
-
+function toTitleCase(sentenceA) {
+  return sentenceA.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
+console.log(toTitleCase(sentenceA))
+
+//output
+console.log(sentenceA)
+console.log(sentenceB)
+console.log(sentenceC)
